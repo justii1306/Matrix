80638aa99c832f8272bc86f96a5db210ced47385
justii1306 <35031460+justii1306@users.noreply.github.com>
2018-01-10 09:58:06 +0100

    Add files via upload

--- matrix.cpp
+++ matrix.cpp
@@ -1,5 +1,51 @@
-include "matrix.h"
-include <stdio.h>
+#include "matrix.h"
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <math.h>
+using namespace std;
 
-matrix::matrix(flat a,float b, float c, float d){
+matrix operator +(matrix m1, matrix m2){
+	int j,i;
+    matrix mw(m1.rows,m1.columns);
+    if(m1.rows!=m2.rows || m1.columns!=m2.columns)
+	throw SizesDoNotMatch();   
+    for(i=0;i<m.rows;i++){
+		for(j=o;j<r1.columns;j++)
+			mw[i][j]=m1[i][j]+m2[i][j];
+	}
+	return mw;}
+
+matrix operator -(matrix m1, matrix m2){
+    if(m1.rows!=m2.rows || m1.columns!=m2.columns)
+	throw SizesDoNotMatch();  
+		throw SizesDoNotMatch();   
+    for(i=0;i<m.rows;i++){
+		for(j=o;j<r1.columns;j++)
+			mw[i][j]=m1[i][j]-m2[i][j]; 
+    
+    }
+
+matrix operator *(matrix m1, matrix m2){
+    if(m1.rows!=m2.columns || m1.columns!=m2.rows)
+	throw SizesDoNotMatch();   
+    
+    }
+
+matrix operator +=(matrix m2){
+    if(this->rows!=m2.rows || this->columns!=m2.columns)
+	throw SizesDoNotMatch();   
+    
+    }
+
+matrix operator -=(matrix m2){
+    if(this->rows!=m2.rows || mthis->columns!=m2.columns)
+	throw SizesDoNotMatch();   
+    
+    }
+
+matrix operator *=(matrix m2){
+    if(this->rows!=m2.columns || this->columns!=m2.rows)
+	throw SizesDoNotMatch();   
+    
     }
--- matrix.h
+++ matrix.h
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
+class SizesDoNotMatch{};
+
 class matrix{
 private:
 public:
-    matrix(float,float,float,float);
+    matrix(int,int);
     friend matrix operator +(matrix,matrix);
     friend matrix operator -(matrix,matrix);
     friend matrix operator *(matrix,matrix);
-    matrix operator +=(matrix);
-    matrix operator -=(matrix);
-    matrix operator *=(matrix);
-    //matrix copy();
+    matrix& operator +=(matrix&);
+    matrix& operator -=(matrix&);
+    matrix& operator *=(matrix&);
+    matrix copy();
+	void operator =(matrix);
+	bool operator ==(matrix, matrix);
+	void operator =();
+	bool operator ==();
+	friend ostream& operator <<(ostream &s, complex &b);
     };
