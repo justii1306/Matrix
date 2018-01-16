f78275db72332f937716ebaf670127c68b6c63ad
Mariusz1306 <33585033+Mariusz1306@users.noreply.github.com>
2018-01-17 00:42:21 +0100

    Add files via upload

---
+++ matrix.cpp
@@ -0,0 +1,178 @@
+#include <math.h>
+#include <fstream>
+#include "Matrix.h"
+
+void matrix::check(unsigned int i){
+	if(block->rows<i)
+		throw IndexOutOfRange();}
+
+matrix::matrix(std::fstream& fs){
+	block = new rcmatrix(fs);}
+
+matrix::matrix(const matrix& cm){
+	cm.block->n++;
+	block = cm.block;}
+
+matrix::~matrix(){
+	if(--block->n==0)
+		delete block;}
+
+matrix::matrix(){
+	block = new rcmatrix();}
+
+matrix::matrix(unsigned int nrows, unsigned int ncols, double var){
+    block = new rcmatrix(nrows,ncols,var);}
+
+matrix& matrix::operator = (const matrix& asOp){
+	asOp.block->n++;
+	if(--block->n == 0)
+		delete block;
+
+	block=asOp.block;
+	return *this;}
+
+std::ostream & operator << (std::ostream & s, const matrix & matrix){
+	//std::cout << "matrix.cols=" << matrix.block->cols <<std::endl;
+	s << "[";
+	for(unsigned int i=0;i<matrix.block->rows;i++){
+		for(unsigned int j=0;j<matrix.block->cols;j++){
+			s << matrix.block->data[i][j];
+			if(((j+1) % matrix.block->cols) == 0 && j!=0 && i!=matrix.block->rows-1)
+				s << "\n ";
+			if(!(i==matrix.block->rows-1 && j==matrix.block->cols-1) && j!=matrix.block->cols-1)
+				s << ", ";
+		}
+	}
+	s << "]";
+	return s;
+}
+
+double matrix::read(unsigned int i, unsigned int j) const{
+    std::cout << "read" << std::endl;
+	try{
+	return block->data[i][j];
+	}
+	catch(...){
+	throw IndexOutOfRange();
+	}
+}
+
+void matrix::write(unsigned int i, unsigned j, double c){
+    std::cout << "write" << std::endl;
+	block = block->detach();
+	try{
+	block->data[i][j] = c;
+	}
+	catch(...){
+	throw IndexOutOfRange();
+	}
+}
+
+matrix::Cref matrix::operator()(unsigned int i, unsigned int j){
+  std::cout << "Cref rcstring::operator[](unsigned int i) CALLED"<<std::endl;
+  check(i);
+  check(j);
+  return Cref(*this,i,j);
+}
+
+std::ostream& operator<<(std::ostream& o, const matrix::Cref& s1){
+std::cout << "operator<<(std::ostream& o, const matrix::Cref& s1)"<<std::endl;
+	o << s1.s.block->data[s1.i][s1.j];
+	return o;}
+
+
+matrix operator +(const matrix& m1, const matrix& m2){
+    if((m1.block->rows!=m2.block->rows)||(m1.block->cols!=m2.block->cols))
+		throw WrongDim();
+
+    matrix result(m1.block->rows,m1.block->cols,0.0);
+
+	for(int i=0; i<result.block->rows; i++){
+		for(int j=0; j<result.block->cols;j++){
+			result.block->data[i][j]=m1.block->data[i][j]+m2.block->data[i][j];}}
+
+    return result;}
+
+
+matrix operator -(const matrix& m1, const matrix& m2){
+    if((m1.block->rows!=m2.block->rows)||(m1.block->cols!=m2.block->cols))
+		throw WrongDim();
+
+    matrix result(m1.block->rows,m1.block->cols,0.0);
+
+	for(int i=0; i<result.block->rows; i++){
+		for(int j=0; j<result.block->cols; j++){
+			result.block->data[i][j]=m1.block->data[i][j]-m2.block->data[i][j];}}
+
+    return result;}
+
+
+matrix& matrix::operator+=(matrix& m2){
+    if((this->block->rows!=m2.block->rows)||(this->block->cols!=m2.block->cols))
+		throw WrongDim();
+
+    this->block = this->block->detach();
+
+    for(int i=0; i<this->block->rows; i++){
+		for(int j=0; j<this->block->cols; j++){
+			this->block->data[i][j]=this->block->data[i][j]+m2.block->data[i][j];}}}
+
+
+matrix& matrix::operator-=(matrix& m2){
+    if((this->block->rows!=m2.block->rows)||(this->block->cols!=m2.block->cols))
+		throw WrongDim();
+
+    this->block = this->block->detach();
+
+    for(int i=0; i<this->block->rows; i++){
+		for(int j=0; j<this->block->cols; j++){
+			this->block->data[i][j]=this->block->data[i][j]-m2.block->data[i][j];}}}
+
+
+matrix operator *(const matrix& m1, const matrix& m2){
+	if((m1.block->rows!=m2.block->rows)||(m1.block->cols!=m2.block->cols))
+		throw WrongDim();
+
+	matrix result(m1.block->rows, m2.block->cols,0.0);
+
+	for(int i=0;i<result.block->rows;i++)
+		for(int j=0;j<result.block->cols;j++){
+			double var=0.0;
+			for(int l=0;l<m1.block->cols;l++)
+				var += m1.block->data[i][l]*m2.block->data[l][j];
+			result.block->data[i][j] = var;}
+
+	return result;}
+
+matrix& matrix:: operator *=(const matrix& m2){
+	if((this->block->rows!=m2.block->rows)||(this->block->cols!=m2.block->cols))
+		throw WrongDim();
+
+	this->block=this->block->detach();
+
+	matrix result(this->block->rows, this->block->cols,0.0);
+
+	for(int i=0;i<result.block->rows;i++)
+		for(int j=0;j<result.block->cols;j++){
+			double var=0.0;
+			for(int l=0;l<this->block->cols;l++)
+				var += this->block->data[i][l]*m2.block->data[l][j];
+			result.block->data[i][j] = var;}
+
+    for(int i=0;i<result.block->rows;i++){
+        for(int j=0;j<result.block->cols;j++){
+            this->block->data[i][j]=result.block->data[i][j];}}
+
+	return *this;}
+
+bool matrix:: operator==(const matrix& m2){
+     if((this->block->rows!=m2.block->rows)||(this->block->cols!=m2.block->cols))
+		return false;
+
+    for(int i=0;i<this->block->rows;i++){
+        for(int j=0;j<this->block->cols;j++){
+            if(this->block->data[i][j]!=m2.block->data[i][j])
+                return false;}}
+
+    return true;}
+
---
+++ matrix.h
@@ -0,0 +1,187 @@
+#include <math.h>
+#include <fstream>
+#include <iostream>
+
+class OutOfMem{};
+class IndexOutOfRange{};
+class WrongDim{};
+
+class matrix{
+	private:
+		struct rcmatrix;
+		rcmatrix* block;
+	public:
+		class Cref;
+
+		matrix();
+		matrix(const matrix& cm);
+		matrix(std::fstream& fs);
+		matrix(unsigned int nrows, unsigned int ncols, double var);
+		matrix(unsigned int nrows, unsigned int ncols, double var1, double var2);
+		void write(unsigned int i, unsigned int j, double c);
+		double read(unsigned int i, unsigned int j) const;
+		double operator()(unsigned int i, unsigned int j) const;
+		matrix& operator=(const matrix& asOp);
+		friend matrix operator +(const matrix&, const matrix&);
+		matrix& operator +=(matrix&);
+		friend matrix operator -(const matrix&, const matrix&);
+		matrix& operator -=(matrix&);
+		friend matrix operator *(const matrix&, const matrix&);
+		matrix& operator *=(const matrix&);
+		bool operator ==(const matrix&);
+		~matrix();
+		friend std::ostream & operator << (std::ostream & s, const matrix & matrix);
+		friend std::ostream & operator << (std::ostream & s, const matrix::Cref& s1);
+		friend matrix operator* (const matrix&, const matrix&);
+		void check(unsigned int i);
+		Cref operator()(unsigned int i, unsigned int j);
+
+};
+
+struct matrix::rcmatrix{
+    double** data;
+    unsigned int rows;
+    unsigned int cols;
+    unsigned int n;
+
+    rcmatrix(){
+        this->data=NULL;
+        this->cols=0;
+        this->rows=0;
+        this->n=0;
+    }
+
+	rcmatrix(std::fstream& fs){
+		fs >> this->rows;
+		fs >> this->cols;
+
+		try{
+			this->data = new double*[this->rows];
+			for(unsigned int i=0;i<this->rows;i++)
+			this->data[i]=new double[this->cols];
+		}
+		catch(...){
+			throw OutOfMem();
+		}
+
+		for(unsigned int i=0;i<this->rows;i++){
+			for(unsigned int j=0;j<this->cols;j++){
+				fs >> this->data[i][j];
+			}
+		}
+		this->n=1;
+	}
+
+	rcmatrix(unsigned int nrows, unsigned int ncols, double var){
+		try{
+			this->data = new double*[nrows];
+			for(unsigned int i=0;i<nrows;i++)
+				this->data[i]=new double[ncols];
+		}
+		catch(...){
+			throw OutOfMem();
+		}
+
+		for(unsigned int i=0;i<nrows;i++)
+			for(unsigned int j=0;j<ncols;j++)
+				data[i][j]=var;
+
+		this->rows=nrows;
+		this->cols=ncols;
+		this->n=1;
+	}
+
+	rcmatrix(unsigned int nrows, unsigned int ncols, double var1, double var2){
+		try{
+			this->data = new double*[nrows];
+			for(unsigned int i=0;i<nrows;i++)
+				this->data[i]=new double[ncols];
+			}
+			catch(...){
+				throw OutOfMem();
+			}
+
+			for(unsigned int i=0;i<nrows;i++)
+				for(unsigned int j=0;j<ncols;j++)
+					if(i==j)this->data[i][j]=var1;
+					else this->data[i][j]=var2;
+
+			this->rows=nrows;
+			this->cols=ncols;
+			this->n=1;
+			};
+
+	rcmatrix(unsigned int nrows, unsigned int ncols,double** l){
+		this->rows=nrows;
+		this->cols=ncols;
+		this->n=1;
+		this->data = l;
+		}
+
+	rcmatrix(const rcmatrix& l){
+		try{
+			this->data = new double*[l.rows];
+			for(unsigned int i=0;i<l.rows;i++)
+				this->data[i]=new double[l.cols];
+		}
+		catch(...){
+			throw OutOfMem();
+		}
+
+		for(unsigned int i=0;i<l.rows;i++)
+			for(unsigned int j=0;j<l.cols;j++)
+				this->data[i][j]=l.data[i][j];
+
+
+		this->rows=l.rows;
+		this->cols=l.cols;
+		this->n=1;
+
+	}
+
+    ~rcmatrix(){
+    for(unsigned int i=0;i<this->rows;i++)
+    	delete[] data[i];
+    delete[] data;
+    }
+
+    rcmatrix* detach(){
+		if(n==1)
+			return this;
+        rcmatrix* t=new rcmatrix(*this);
+        n--;
+        return t;
+    }
+};
+
+class matrix::Cref{
+	friend class matrix;
+	matrix& s;
+	unsigned int i;
+	unsigned int j;
+
+	Cref (matrix& ss, unsigned int ii, unsigned int jj): s(ss), i(ii), j(jj){
+		std::cout << "cref contructor CALLED"<<std::endl;
+	};
+
+	public:
+		operator double() const{
+			std::cout << "operator double CALLED"<<std::endl;
+			return s.read(i, j);
+		};
+
+		matrix::Cref& operator = (double c){
+			std::cout << "operator = (double c) CALLED" << std::endl;
+			s.write(i, j, c);
+			return *this;
+		};
+
+		matrix::Cref& operator = (const Cref& ref){
+			std::cout << "operator = (const Cref& ref) CALLED" << std::endl;
+			return operator = (ref);
+		};
+
+		friend std::ostream& operator<<(std::ostream&, const matrix::Cref&);
+};
+
+
